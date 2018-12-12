ae8fbb71f7d584c3e0680685b3a940ec72ebc545
JackyRecelis <jackymlui@live.com>
2018-12-12 16:38:24 +1000

    changed port to 3000 for windows

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static('public'))
 
-app.listen(process.envPORT|| 8080, ()=>{
+app.listen(process.envPORT|| 3000, ()=>{
     console.log("All is ok");
+})
+
+app.get('/newpage', ()=>{
+    res.send("this is a new page");
 })
